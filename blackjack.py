b1632656907f1fc3c5e0f5c7a72865ed32853944
adam <adavenpo@purdue.edu>
2017-04-13 17:51:50 -0400

    Removing players into separate class

--- blackjack.py
+++ blackjack.py
@@ -1,14 +1,5 @@
+from player import Player, Ai_Player
 import cards
-
-class Player():
-    def __init__(self, name):
-        self.hand = []
-        self.name = name
-    def discard(self):
-        self.hand = []
-        return cards
-    def draw(self, card):
-        self.hand.append(card)
 
 class Game():
     def __init__(self, players):
@@ -16,31 +7,25 @@ class Game():
         self.players = []
         for p in players:
             self.players.append(Player(p))
-    def calculate_score(self, hand):
-        score = 0
-        for c in hand:
-            value = c.value()
-            if value != 11:
-                score += value
-            else:
-                if score + 11 > 21:
-                    score += 1
-                else:
-                    score += 11
-        return score
     def reset_round(self):
         self.deck = Deck()
         for p in self.players:
             p.discard()
-    def draw(self):
-        return self.deck.draw()
+    def draw(self, player):
+        card = self.deck.draw()
+        player.draw(card)
+        print(player.name + ' drew {} of {}'.format(card.rank, card.suite))
+
     def deal(self):
         for i in range(2):
             for p in self.players:
-                c = self.draw()
+                c = self.deck.draw()
                 p.hand.append(c)
     def find_winner(self):
+        scores = {}
         for p in self.players:
+            scores[p.name] = p.calculate_score()
+        print(scores)
 
     def play_game(self):
         print('The game is begining with {} players.'.format(len(self.players)))
@@ -49,8 +34,10 @@ class Game():
         self.deal()
         for p in self.players:
             inp = ''
-            while inp != 'd' or inp != 'p':
-                print('Value of your current hand: {}. Would you like to draw (d) or pass (p)?'.format(self.calculate_score(p.hand)))
+            while inp != 'd' and inp != 'p':
+                p.view_hand()
                 inp = input().lower()
-                if inp == 'y':
-                    p.draw(self.draw())
+                if inp == 'd':
+                    self.draw(p)
+                    inp = ''
+        self.find_winner()
