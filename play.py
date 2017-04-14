e15825987d1a3395d3a5db56abb6964376e85820
adam <adavenpo@purdue.edu>
2017-04-14 12:57:28 -0400

    Formatting playercount input

--- play.py
+++ play.py
@@ -1,5 +1,19 @@
 from blackjack import Game
 
 print('Welcome to blackjack')
-game = Game(['Adam'])
+print('Please enter your name:')
+name = input()
+print('How many other players should be in the game?')
+while True:
+    try:
+        player_count = int(input())
+        if player_count >= 0 and player_count < 6:
+            break
+        else:
+            print('Please enter a valid number between 1 and 5:')
+    except ValueError:
+        print('Please enter a valid number between 1 and 5:')
+
+
+game = Game(name, player_count)
 game.play_game()
